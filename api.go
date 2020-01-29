0d43f85110fc16499c2bec78ccf00f0e11d4f106
Jim Sheldon <james.sheldon@wexinc.com>
2020-01-29 00:45:17 -0500

    MITM proxy fix

--- api.go
+++ api.go
@@ -24,9 +24,14 @@ func (g *Geocodio) Call(path string, query map[string]string) (GeocodeResult, er
 	}
 
 	timeout := time.Duration(10 * time.Second)
+  tr := &http.Transport{
+     TLSClientConfig: &tls.Config{InsecureSkipVerify: true},
+  }
 	client := http.Client{
 		Timeout: timeout,
+    Transport: tr,
 	}
+
 
 	resp, err := client.Get(_url)
 
